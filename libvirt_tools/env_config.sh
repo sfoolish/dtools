a9a1f28a74a3ea6309d534a7c17d2e318ce50247
sfoolish <sfoolish.liang@gmail.com>
2016-01-22 17:23:24 +0800

    Make VM parameters configurable throught env
    Signed-off-by: sfoolish <sfoolish.liang@gmail.com>

--- libvirt_tools/env_config.sh
+++ libvirt_tools/env_config.sh
@@ -10,10 +10,10 @@
 # this is a modified copy of bottlenecks/utils/rubbos_dev_env_setup/env_config.sh
 
 export HOSTNAMES=${HOSTNAMES:-"ubuntu"}
-export VIRT_NUMBER=9
-export VIRT_MEM=4096
-export VIRT_CPUS=4
+export VIRT_NUMBER=${VIRT_NUMBER:-"1"}
+export VIRT_MEM=${VIRT_MEM:-"4096"}
+export VIRT_CPUS=${VIRT_CPUS:-"4"}
 export IMAGE_URL=${IMAGE_URL:-"https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img"}
-export IMAGE_NAME=disk.img
-export IPADDR_PREFIX="192.168.122."
+export IMAGE_NAME=${IMAGE_NAME:-"disk.img"}
+export IPADDR_PREFIX=${IPADDR_PREFIX:-"192.168.122."}
 
