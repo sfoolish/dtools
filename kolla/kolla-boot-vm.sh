839c720cce2a97c1339c02c1b8f4823aa32789de
sfoolish <sfoolish.liang@gmail.com>
2016-12-20 16:04:28 -0800

    support kolla multinode deploy
    update final boot_vm test script.
    still need to reconfigure glance backend storage.
    
    Signed-off-by: sfoolish <sfoolish.liang@gmail.com>

--- kolla/kolla-boot-vm.sh
+++ kolla/kolla-boot-vm.sh
@@ -4,6 +4,26 @@ source /etc/kolla/admin-openrc.sh
 
 ifconfig eth1 10.0.2.2/24
 
+# tmp sync glance cirros image
+
+image_id=$(openstack image list | grep cirros | awk '{print $2}')
+
+for i in control01 control02 control03; do
+    ssh $i ls /var/lib/docker/volumes/glance/_data/images/$image_id
+    if [ $? -ne 0 ]; then
+        continue
+    fi
+
+    for j in control01 control02 control03; do
+        if [ $i == $j ]; then
+            continue
+        fi
+        ssh $i "scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /var/lib/docker/volumes/glance/_data/images/$image_id $j:/var/lib/docker/volumes/glance/_data/images"
+    done
+    echo "Glance Image Synced"
+    break
+done
+
 openstack server create \
     --image cirros \
     --flavor m1.tiny \
--- kolla/run_kolla_multinode_test.sh
+++ kolla/run_kolla_multinode_test.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
+date
+
+cd ../libvirt_tools/
+./deploy.sh
+cd -
+
+date
+
 ansible -i inventory all -m copy -a "src=/root/.ssh/id_rsa dest=/root/.ssh/id_rsa mode=600"
 ansible -i inventory all -m copy -a "src=/root/.ssh/id_rsa.pub dest=/root/.ssh/id_rsa.pub mode=644"
 ansible -i inventory all -m shell -a 'ifconfig eth1 up'
 
+date
+
 ansible -i inventory ansible -m copy -a "src=kolla-multinode1.sh dest=~/kolla-multinode1.sh mode=777"
 ansible -i inventory ansible -m shell -a '~/kolla-multinode1.sh'
+
+date
 
 ansible -i inventory kolla -m copy -a "src=kolla-multinode2.sh dest=~/kolla-multinode2.sh mode=777"
 ansible -i inventory kolla -m shell -a '~/kolla-multinode2.sh'
 
+date
 
 ansible -i inventory ansible -m shell -a "ANSIBLE_HOST_KEY_CHECKING=False kolla-ansible prechecks -i /usr/share/kolla/ansible/inventory/multinode"
 
+date
+
 ansible -i inventory ansible -m shell -a "ANSIBLE_HOST_KEY_CHECKING=False kolla-ansible deploy -i /usr/share/kolla/ansible/inventory/multinode"
+
+date
 
 ansible -i inventory ansible -m copy -a "src=kolla-init-runonce.sh dest=~/kolla-init-runonce.sh mode=777"
 ansible -i inventory ansible -m shell -a '~/kolla-init-runonce.sh'
+
+date
+
+ansible -i inventory ansible -m copy -a "src=kolla-boot-vm.sh dest=~/kolla-boot-vm.sh mode=777"
+ansible -i inventory ansible -m shell -a '~/kolla-boot-vm.sh'
+
+date
 
