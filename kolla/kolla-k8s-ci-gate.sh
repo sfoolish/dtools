b49a3103ebcd1a5ee3d8a8c66fc25e3fef878a00
sfoolish <sfoolish.liang@gmail.com>
2016-12-29 18:00:35 -0800

    Revert "Revert "add script to run kolla-kubernetes ci gate locally""
    This reverts commit 514823efa8734d8549947dd72e624ec6efda9e75.

---
+++ kolla/kolla-k8s-ci-gate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# http://logs.openstack.org/71/415171/1/check/gate-kolla-kubernetes-deploy-centos-binary-ceph-nv/4725106/console.html
+# http://www.tecmint.com/setup-dns-cache-server-in-centos-7/
+
+yum install -y epel-release
+
+yum install -y crudini cyrus-sasl-devel jq libcurl-devel libffi-devel openssl-devel python34-devel
+
+yum install -y git vim unbound python-virtualenv ansible
+yum group install -y "Development Tools"
+
+ifconfig eth1 192.168.222.2/24
+
+mkdir -p /etc/nodepool
+echo "192.168.222.2" > /etc/nodepool/primary_node_private
+
+export WORKSPACE=/home/jenkins/workspace/gate-kolla-kubernetes-deploy-centos-binary-ceph-nv
+
+mkdir -p $WORKSPACE
+
+cd $WORKSPACE
+
+git clone https://github.com/openstack/kolla-kubernetes.git .
+
+mkdir -p $WORKSPACE/logs/
+
+sed  -ie "s/180/360/g" tools/wait_for_pods.sh
+
+cat << EOF > tests/bin/fix_gate_iptables.sh
+#!/bin/bash -xe
+
+echo "cleaned by sf"
+
+EOF
+tools/setup_gate.sh deploy centos binary ceph centos-7 shell
+
