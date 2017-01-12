cffa1fe71e19b346748015eccafe2a929fde78ba
sfoolish <sfoolish.liang@gmail.com>
2017-01-11 19:47:06 -0800

    generate local image index.html
    Signed-off-by: sfoolish <sfoolish.liang@gmail.com>

--- libvirt_tools/image_download.sh
+++ libvirt_tools/image_download.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+LOCAL_BASE_URL="http://192.168.21.2:8888"
 IMAGE_URL="http://cloud.centos.org/centos/7/atomic/images/CentOS-Atomic-Host-7-GenericCloud.qcow2 "
 IMAGE_URL+="http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2 "
 IMAGE_URL+="https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img "
@@ -7,5 +8,11 @@ IMAGE_URL+="https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudim
 
 for url in $IMAGE_URL; do
     curl -O $url
+done
+
+# genarate image links
+rm -rf index.html
+for i in $(ls -l | grep -v image | grep -v tot | awk '{print $9}'); do
+    echo ${LOCAL_BASE_URL}/${i} >> index.html
 done
 
