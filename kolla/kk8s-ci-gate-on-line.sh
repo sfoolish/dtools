3ac8b65821521888730ca86270ce9fd1e5b491b8
sfoolish <sfoolish.liang@gmail.com>
2017-01-17 19:47:05 +0800

    add base image create
    Signed-off-by: sfoolish <sfoolish.liang@gmail.com>

--- kolla/kk8s-ci-gate-on-line.sh
+++ kolla/kk8s-ci-gate-on-line.sh
@@ -33,6 +33,33 @@ cat << EOF > tests/bin/fix_gate_iptables.sh
 echo "cleaned by sf"
 
 EOF
-tools/setup_gate.sh deploy centos binary ceph centos-7 shell
+
+if [ "x$CREATE_BASE_IMG" == "xbase" ]; then
+    sudo yum remove -y iscsi-initiator-utils
+    sudo yum install -y bridge-utils
+
+    # install docker kubelet
+    setenforce 0
+    cat <<"EOEF" > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=http://yum.kubernetes.io/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOEF
+    yum install -y docker kubelet kubeadm kubectl kubernetes-cni ebtables
+    systemctl start kubelet
+    systemctl start docker
+
+    cat docker_images.txt | while read image; do
+        docker pull $image
+    done
+else
+    tools/setup_gate.sh deploy centos binary ceph centos-7 shell
+fi
 
 echo "source <(kubectl completion bash)" >> ~/.bashrc
+
