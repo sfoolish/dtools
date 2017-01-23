6dd3366b42debb7eb19aa0768b8977875bd37e20
sfoolish <sfoolish.liang@gmail.com>
2017-01-23 02:03:25 -0800

    support multi nodes base image creation
    Signed-off-by: sfoolish <sfoolish.liang@gmail.com>

--- kolla/kk8s-base-image-boot.sh
+++ kolla/kk8s-base-image-boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HOST_NAME=kmm0 IMAGE_NAME=disk.img_image-pulled-kmm0 ./_kk8s-base-image-boot.sh
-HOST_NAME=kmn1 IMAGE_NAME=disk.img_image-pulled-kmn1 ./_kk8s-base-image-boot.sh
-HOST_NAME=kmn2 IMAGE_NAME=disk.img_image-pulled-kmn2 ./_kk8s-base-image-boot.sh
+./_kk8s-base-image-boot.sh kmm0 disk.img_image-pulled-kmm0
+./_kk8s-base-image-boot.sh kmn1 disk.img_image-pulled-kmn1
+./_kk8s-base-image-boot.sh kmn2 disk.img_image-pulled-kmn2
 
