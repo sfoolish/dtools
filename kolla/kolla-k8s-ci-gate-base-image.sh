e99ff80c93d18bc62d960ff79104b0419f45f1fb
sfoolish <sfoolish.liang@gmail.com>
2017-02-08 07:38:34 -0800

    minor fix
    Signed-off-by: sfoolish <sfoolish.liang@gmail.com>

--- kolla/kolla-k8s-ci-gate-base-image.sh
+++ kolla/kolla-k8s-ci-gate-base-image.sh
@@ -15,7 +15,7 @@ cd /home/jenkins/workspace/gate-kolla-kubernetes-deploy-centos-binary-ceph-nv/
 git checkout ./
 git pull
 
-sed -i -e "s/181/360/g" tools/wait_for_pods.sh
+sed -i -e "s/180/360/g" tools/wait_for_pods.sh
 sed -i -e "s/240/480/g" tools/setup_gate.sh
 sed -i -e "s/240/480/g" tools/setup_gate_iscsi.sh
 sed -i -e "s/240/480/g" tools/setup_rbd_volumes.sh
