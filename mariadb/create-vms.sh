1c21e24efc8ba4e771b4cbc6e245adf82a23b750
sfoolish <sfoolish.liang@gmail.com>
2017-03-20 23:43:43 -0700

    add mariadb galera cluster test scripts
    ansible role copy from compass4nfv, commit id:
        b7fb5c062e0e806e34ebc53fe670f00f8417cd4a
    
    This patch only test on xenial
    
    Signed-off-by: sfoolish <sfoolish.liang@gmail.com>

---
+++ mariadb/create-vms.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+
+source libvirt-env-xenial
+
+# DATE=$(date +"%y-%m-%d-%T")
+DATE=$(date +"%y-%m-%d-%H")
+LOG_DIR=/tmp/k8s-ci-${DATE}
+
+mkdir -p ${LOG_DIR}
+
+# deploy vms
+pushd ../libvirt_tools
+./deploy.sh 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' | tee -a ${LOG_DIR}/libvirt_deploy.log
+popd
+
+ssh $KMASTER "hostname"
+if [ $? != 0 ]; then
+  echo "!!! Deploy Failed EXIT !!!" >> ${LOG_DIR}/libvirt_deploy.log
+  exit 1
+fi
+
